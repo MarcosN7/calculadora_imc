9c29c1172847114fc276d5a53d985f8c2f1d468b
MarcosN7 <marcosbastos67@gmail.com>
2023-08-18 14:43:42 -0400

    Add code for BMI Calculator app with Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(
+      appDocumentDir.path); // Use appDocumentDir.path instead of appDocumentDir
+  await Hive.openBox<IMCData>('imcBox');
   runApp(const MyApp());
-}
-
-class IMC {
-  double weight = 0;
-  double height = 0;
-  double result = 0;
-  String interpretation = "";
-
-  IMC(this.weight, this.height);
 }
 
 class MyApp extends StatelessWidget {
@@ -28,6 +26,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
+@HiveType(typeId: 1)
+class IMCData extends HiveObject {
+  @HiveField(0)
+  double weight = 0;
+
+  @HiveField(1)
+  double height = 0;
+
+  @HiveField(2)
+  double result = 0;
+
+  @HiveField(3)
+  String interpretation = '';
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -36,38 +49,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  IMC imc = IMC(0, 0);
-  List<IMC> imcList = [];
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
 
-  void calculateBMI() {
-    if (imc.weight > 0 && imc.height > 0) {
-      double bmi = imc.weight / (imc.height * imc.height);
-      setState(() {
-        imc.result = bmi;
-
-        if (bmi < 16) {
-          imc.interpretation = "Magreza grave";
-        } else if (bmi < 17) {
-          imc.interpretation = "Magreza moderada";
-        } else if (bmi < 18.5) {
-          imc.interpretation = "Magreza leve";
-        } else if (bmi < 25) {
-          imc.interpretation = "Saudável";
-        } else if (bmi < 30) {
-          imc.interpretation = "Sobrepeso";
-        } else if (bmi < 35) {
-          imc.interpretation = "Obesidade Grau I";
-        } else if (bmi < 40) {
-          imc.interpretation = "Obesidade Grau II";
-        } else {
-          imc.interpretation = "Obesidade Grau III";
-        }
-
-        imcList.add(imc);
-        imc = IMC(0, 0); // Limpa os campos após o cálculo
-      });
-    }
-  }
+  // ...
 
   @override
   Widget build(BuildContext context) {
@@ -81,51 +66,25 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const SizedBox(height: 16.0),
-              TextField(
-                onChanged: (value) => imc.weight = double.tryParse(value) ?? 0,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Peso (kg)',
+              // ... Other widgets
+
+              Expanded(
+                child: ValueListenableBuilder<Box<IMCData>>(
+                  valueListenable: Hive.box<IMCData>('imcBox').listenable(),
+                  builder: (context, box, _) {
+                    return ListView.builder(
+                      itemCount: box.length,
+                      itemBuilder: (context, index) {
+                        IMCData imcData = box.getAt(index)!;
+                        return ListTile(
+                          title:
+                              Text('IMC: ${imcData.result.toStringAsFixed(1)}'),
+                          subtitle: Text(imcData.interpretation),
+                        );
+                      },
+                    );
+                  },
                 ),
-              ),
-              const SizedBox(height: 16.0),
-              TextField(
-                onChanged: (value) => imc.height = double.tryParse(value) ?? 0,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Altura (m)',
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: calculateBMI,
-                child: const Text('Calcular'),
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                'IMC: ${imc.result.toStringAsFixed(1)}',
-                style: const TextStyle(fontSize: 24),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                imc.interpretation,
-                style: const TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16.0),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: imcList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                        'IMC: ${imcList[index].weight} / ${imcList[index].height}'),
-                    subtitle: Text(
-                        'Resultado: ${imcList[index].result.toStringAsFixed(1)} - ${imcList[index].interpretation}'),
-                  );
-                },
               ),
             ],
           ),
